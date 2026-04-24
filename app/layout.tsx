324a555ce9578f9755d37af3a7fcc1cdae4a90e3
Artur <artur21kim@gmail.com>
2026-04-23 21:57:35 -0700

    Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from 'next'
 import './globals.css'
 import { AuthProvider } from './providers/AuthProvider'
@@ -27,6 +28,7 @@ export default function RootLayout({
           </AuthProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
