84b95aa3e12730aa8403890db8a38887170436b7
Artur <artur21kim@gmail.com>
2026-06-12 14:37:18 -0700

    feat(discover): swap OSM tiles for CartoDB Dark Matter basemap on venue map (SCRUM-84)

--- app/discover/upcoming/VenueMap.tsx
+++ app/discover/upcoming/VenueMap.tsx
@@ -86,8 +86,8 @@ export default function VenueMap({ shows }: { shows: Show[] }) {
         scrollWheelZoom
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
         {venuePins.map(pin => {
           const color   = pin.hasSpotifyMatch ? TEAL : GRAY
