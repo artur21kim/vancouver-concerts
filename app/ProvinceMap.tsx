406ee9f2377a4920fa75c9996392ca5f1511afb3
Artur <artur21kim@gmail.com>
2026-08-28 03:36:25 -0700

    fix(map): swap Carto tile URL from rastertiles/voyager to light_all CDN (GP-199)

--- app/ProvinceMap.tsx
+++ app/ProvinceMap.tsx
@@ -273,7 +273,7 @@ export default function ProvinceMap({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         <MapController selectedState={selectedState} selectedProvince={selectedProvince} />
--- app/discover/upcoming/VenueMap.tsx
+++ app/discover/upcoming/VenueMap.tsx
@@ -261,7 +261,7 @@ export default function VenueMap({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         {/* Non-match pins first, Spotify-match pins on top */}
